c85986fb077a8929e6a387bacbc00df3ea756b83
Lekx <hsaul.sanchez@alumno.udg.mx>
2013-10-29 16:11:03 -0600

    First Upload

---
+++ currencyConverter/tipoCambio.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace currencyConverter
+{
+    class tipoCambio
+    {
+
+
+        public double conversionPesoADolar(string peso, string cambio) 
+        {
+            DialogResult mensaje;
+            double pesoConvertido;
+            double cambioConvertido;
+
+            
+
+            if (peso != "" && cambio != "")
+            {
+                if (peso != "0" && cambio != "0")
+                {
+                    if (Double.TryParse(peso, out pesoConvertido) && Double.TryParse(cambio, out cambioConvertido))
+                    {
+                        return pesoConvertido / cambioConvertido;
+                    }
+                    else
+                    {
+                        mensaje = MessageBox.Show("Oops! Solo se aceptan valores numericos.");
+                        return 0;
+                    }
+                }
+                else
+                {
+                       
+                    mensaje = MessageBox.Show("Oops! Ninguno de los campos puede ser 0.");
+                    return 0;
+                }
+            }
+            else
+            {
+                mensaje = MessageBox.Show("Oops! Ninguno de los campos pueden estar vacios.");
+                return 0;
+            }
+       
+        }
+    }
+}
+
